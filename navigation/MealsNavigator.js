678b369f1370d44467c52cc1b77a3f329d62232d
gaoliver <gabriel.o.c.ramos@gmail.com>
2021-01-05 18:23:46 -0300

    Screens + Drawer Nav

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -6,10 +6,13 @@ import { createBottomTabNavigator } from "react-navigation-tabs";
 import { Ionicons, MaterialIcons } from "@expo/vector-icons";
 import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
 
+// Screens
 import CategoriesScreen from "../Screen/CategoriesScreen";
 import CategoryMealsScreen from "../Screen/CategoryMealsScreen";
 import MealDetailScreen from "../Screen/MealDetailScreen";
 import FavoritesScreen from "../Screen/FavoritesScreen";
+import FiltersScreen from "../Screen/FiltersScreen";
+import HomeScreen from "../Screen/HomeScreen";
 import ProfileScreen from "../Screen/ProfileScreen";
 import SettingsScreen from "../Screen/SettingsScreen";
 import FollowingScreen from "../Screen/FollowingScreen";
@@ -18,27 +21,90 @@ import HelpScreen from "../Screen/HelpScreen";
 
 import Colors from "../Constants/Colors";
 
-const MealsNavigator = createStackNavigator({
-  Categories: CategoriesScreen,
-  "Category Meals": {
-    screen: CategoryMealsScreen,
-    navigationOptions: {},
+const defaultStackNavOptions = (navData) => {
+  return {
+          headerRight: (
+            <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
+              <Item
+                title="Profile"
+                iconName="person"
+                iconSize={25}
+                onPress={() => {
+                  navData.navigation.toggleDrawer();
+                }}
+              />
+            </HeaderButtons>
+          ),
+  }
+}
+
+const MealsNavigator = createStackNavigator(
+  {
+    Categories: CategoriesScreen,
+    "Category Meals": {
+      screen: CategoryMealsScreen,
+    },
+    MealDetail: { screen: MealDetailScreen },
   },
-  MealDetail: MealDetailScreen,
-});
+  {
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+);
+
+const HomeNavigator = createStackNavigator(
+  {
+    "Food App": { screen: HomeScreen },
+  },
+  {
+    defaultNavigationOptions: defaultStackNavOptions
+  }
+);
+
+const FilterNavigator = createStackNavigator(
+    {
+        Filters: FiltersScreen
+    },
+    {
+        navigationOptions: {},
+        defaultNavigationOptions: defaultStackNavOptions
+    }
+)
 
 const FavNavigator = createStackNavigator({
-  "My favorites": {screen: FavoritesScreen},
-  "Meal detail": MealDetailScreen,
+  "My favorites": { screen: FavoritesScreen },
+  MealDetail: MealDetailScreen,
+},
+{
+    defaultNavigationOptions: defaultStackNavOptions
 });
 
 const tabScreenConfig = {
+  Home: {
+    screen: HomeNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return (
+          <MaterialIcons name="home-filled" size={25} color={tabInfo.tintColor} />
+        );
+      },
+    },
+  },
   Meals: {
     screen: MealsNavigator,
     navigationOptions: {
       tabBarIcon: (tabInfo) => {
         return (
-          <Ionicons name="ios-restaurant" size={25} color={tabInfo.tintColor} />
+          <Ionicons name="restaurant" size={22} color={tabInfo.tintColor} />
+        );
+      },
+    },
+  },
+  Filters: {
+    screen: FilterNavigator,
+    navigationOptions: {
+      tabBarIcon: (tabInfo) => {
+        return (
+          <MaterialIcons name="search" size={25} color={tabInfo.tintColor} />
         );
       },
     },
@@ -60,9 +126,10 @@ const MealsFavTabNavigator =
     ? createMaterialBottomTabNavigator(tabScreenConfig, {
         activeColor: Colors.text,
         shifting: true,
-        labeled: true,
+        labeled: false,
         barStyle: {
           backgroundColor: Colors.primary,
+          height: 50,
         },
       })
     : createBottomTabNavigator(tabScreenConfig, {
@@ -77,4 +144,26 @@ const MealsFavTabNavigator =
         },
       });
 
-export default createAppContainer(MealsFavTabNavigator);
+
+      const FiltersNavigator = createStackNavigator({
+        Filters: FiltersScreen
+      })
+
+
+
+HomeScreen.navigationOptions = {
+    headerTitleStyle: {
+        fontFamily: 'hipster',
+        fontSize: 40
+    }
+}
+
+
+const profileNavigator = createDrawerNavigator({
+  MealsFav: MealsFavTabNavigator,
+  Filters: FiltersNavigator
+});
+      
+
+export default createAppContainer(profileNavigator);
+
